e99513a0ceb28e1f32b70d56282b0d3da88cbee3
palak178 <70845267+palak178@users.noreply.github.com>
2021-09-22 20:28:13 +0530

    Update matrixsum.java

--- matrixsum.java
+++ matrixsum.java
@@ -1,30 +1,20 @@
 package mycap;
 import java.util.*;
-public class MyCap {
-    public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter order of square matrix:");
-    int n = input.nextInt();
-    int[][] x = new int[n][n];
-    int[][] y = new int[n][n];
-    int[][] z = new int[n][n];
-    System.out.println("Please enter first matrix - ");
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                x[i][j] = input.nextInt();
-    System.out.println("Please enter second matrix - ");
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                y[i][j] = input.nextInt();
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            z[i][j] = x[i][j] + y[i][j];
-    System.out.println("Sum of the given matrices is ");
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-            System.out.print(z[i][j]+" ");
-        System.out.println();
-    }
-    input.close();
-}
+public class MyCap{  
+public static void main(String args[]){  
+//creating two matrices    
+int a[][]={{1,3,4},{2,4,3},{3,4,5}};    
+int b[][]={{1,3,4},{2,4,3},{1,2,4}};    
+    
+//creating another matrix to store the sum of two matrices    
+int c[][]=new int[3][3];  //3 rows and 3 columns  
+    
+//adding and printing addition of 2 matrices    
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+c[i][j]=a[i][j]+b[i][j];    
+System.out.print(c[i][j]+" ");    
+}    
+System.out.println();//new line    
+}    
+}}  
